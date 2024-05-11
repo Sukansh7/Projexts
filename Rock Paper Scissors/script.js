f81bee375bfd550b309fbe88f2197b9ba3f7853f
Sukansh Gupta <sukansh.07@gmail.com>
2024-05-11 17:54:16 +0530

    committed

---
+++ Rock Paper Scissors/script.js
@@ -0,0 +1,57 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice")
+const msg = document.querySelector("#msg")
+const userScoreVal = document.querySelector("#user-score")
+const compScoreVal = document.querySelector("#comp-score")
+
+const drawGame = () => {
+    msg.innerText = "Game was a DRAW"
+    msg.style.backgroundColor = "#081b31"
+}
+
+const showWinner = (userWin) => {
+    if (userWin) {
+        userScore++
+        userScoreVal.innerText = userScore
+        msg.innerText = "You WIN!";
+        msg.style.backgroundColor = "green"
+    } else {
+        compScore++
+        compScoreVal.innerText = compScore
+        msg.innerText = "You LOSE";
+        msg.style.backgroundColor = "red"
+    }
+}
+
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissors"]
+    const randIdx = Math.floor(Math.random() * 3)
+    return options[randIdx]
+}
+
+const playGame = (userChoice) => {
+    const compChoice = genCompChoice()
+
+    if (userChoice === compChoice) {
+        drawGame()
+    } else {
+        let userWin = true;
+        if (userChoice === "rock") {
+            userWin = compChoice === "paper" ? false : true;
+        } else if (userChoice === "paper") {
+            userWin = compChoice === "scissors" ? false : true;
+        } else {
+            userWin = compChoice === "rock" ? false : true;
+        }
+        showWinner(userWin)
+    }
+}
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id")
+        playGame(userChoice)
+    })
+})
